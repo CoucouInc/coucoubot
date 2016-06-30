e161b9dbd652698a3818bbfaa126810cb4b03c34
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-06-30 22:05:27 +0200

    fix import script

--- tools/load_dump.ml
+++ tools/load_dump.ml
@@ -11,8 +11,7 @@
 
 let s = CCIO.with_in "old_dump" CCIO.read_lines_l;;
 let l = CCList.filter_map Factoids.parse_op s;;
-Prelude.StrMap.cardinal fs;;
 let fs = List.fold_left (fun acc (Factoids.Set f) -> Factoids.append f acc) Factoids.empty l;;
-Prelude.StrMap.cardinal fs;;
-Factoids.write_file ~file:"factoids.json" fs;;
+Printf.printf "%d records\n" (Prelude.StrMap.cardinal fs);;
+Factoids.write_file ~file:"factoids.json" fs |> Lwt_main.run;;
 
