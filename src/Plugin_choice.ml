8c822bbe05e679ddc49cb1ae2114c081a957076d
Antoine Pouille <antoine.pouille@datadoghq.com>
2018-04-10 20:30:01 +0200

    Adding !battle, support if no separators are found, and multi-command support

--- src/Plugin_choice.ml
+++ src/Plugin_choice.ml
@@ -2,7 +2,7 @@ open Calculon
 open Prelude
 
 (** Separate a string into n parts using the given separator **)
-let extract_choices sep str =
+let extract_choices_sep sep str =
   let sep = String.lowercase_ascii sep in
   let rec list_to_string l =
     match l with
@@ -45,30 +45,52 @@ let rec extract_choices_seps sep_list str =
   match sep_list with
     | [] -> None
     | s::ss ->
-      match extract_choices s str with
+      match extract_choices_sep s str with
         | None -> extract_choices_seps ss str
         | Some c -> Some c
 
-let cmd_choice =
-  let supported_separators = ["vs"; "|"; "||"; "&"; "&&"; "or"; "and"; "ou"; "et";
-    "contre"; "versus"] in
-  let cmd_function =
+(** Just separate the string with spaces, used if no separators work **)
+let extract_basic_choices str =
+  match String.split_on_char ' ' str |> List.filter (fun s -> s <> "") with
+    | [] -> None
+    | [_] -> None
+    | l -> Some l
+
+let extract_choices sep_list str =
+    match extract_choices_seps sep_list str with
+      | Some c -> Some c
+      | None -> extract_basic_choices str
+
+let supported_separators = ["|"; "||"; "&"; "&&"; "vs"; "or"; "and"; "ou"; "et";
+  "contre"; "versus"]
+
+(* Gives an arbitrary choice based on a arbitrary hash: gives a total order *)
+let determinate_choice l =
+    List.sort (fun a b -> Hashtbl.hash a - (Hashtbl.hash b)) l |> List.hd
+
+(* setup choice commands *)
+let cmds_choice =
+  (* create a choice command using a specific choice_function *)
+  let cmd_function choice_function result_message failure_message =
     (fun _ str ->
-       let choice_opt =
-         String.trim str
-         |> extract_choices_seps supported_separators
-         |> map_opt random_l in
-      let msg = choice_opt
-        |> map_opt (fun s -> "Et c'est décidé ! Ce sera " ^ s)
-        |? "Malheureusement, il n'est pas possible de parser votre indécision, même avec les dernières percées de nos IA multi-cloud-ready bare metal. Faîtes un effort !"
-      in
+       let choice_opt = String.trim str
+         |> extract_choices supported_separators |> map_opt choice_function in
+      let msg = choice_opt |> map_opt (fun s -> result_message ^ s) |? failure_message in
       Lwt.return (some msg)
     )
   in
-  Command.make_simple
-    ~descr:"Faire un choix entre deux options"
-    ~prefix:"choix"
-    ~prio:10
-    cmd_function
+  (* easily define a choice command *)
+  let command_make_choice command_name command_descr choice_function result_message failure_message =
+      Command.make_simple ~descr:command_descr ~prefix:command_name ~prio:10
+        (cmd_function choice_function result_message failure_message)
+  in
 
-let plugin = Plugin.of_cmd cmd_choice
+  [command_make_choice "choix" "Faire un choix entre deux options" random_l "Et c'est décidé ! Ce sera "
+     "Malheureusement, il n'est pas possible de parser votre indécision, même avec les dernières percées de nos IA multi-cloud-ready bare metal. Faites un effort !";
+   command_make_choice "choice" "Make a choice between two options" random_l "And it's decided! It's gonna be "
+     "Sorry, unable to parse your query here. Please consider upgrading your bot to a modern language like golang or ruby ;). Who still uses OCaml nowadays?";
+   command_make_choice "battle" "Qui que c'est le plus fort ?" determinate_choice
+     "Évidemment, ça va être " "Pas facile de dire… Z'êtes sûrs de la question ?"
+  ]
+
+let plugin = Plugin.of_cmds cmds_choice
