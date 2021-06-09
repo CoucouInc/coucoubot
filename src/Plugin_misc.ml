1daed1e9fc464f43669e3155d1b34c6e45839628
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2021-06-09 16:49:49 +0200

    Add a !how command, fetching a randow wikihow article

--- src/Plugin_misc.ml
+++ src/Plugin_misc.ml
@@ -42,6 +42,36 @@ let cmd_cancer =
        )
     )
 
+let wikihow_random =
+  "https://www.wikihow.com/Special:Randomizer"
+
+let cmd_how =
+  Command.make_simple
+    ~prio:10 ~cmd:"how" ~descr:"how do I do things??"
+    (fun _ _ ->
+       Logs.debug (fun k->k "!how (now querying)");
+       Lwt_preemptive.detach (fun () ->
+         Curly.run ~args:["-L"]
+           Curly.(Request.make ~url:wikihow_random ~meth:`GET ())) ()
+       >>= function
+       | Ok {Curly.Response.code; headers; _} ->
+         Logs.debug (fun k->k "how: got answer (code: got %d; expected 302)" code);
+         if code = 302 then (
+           match List.assoc_opt "location" headers with
+           | Some url ->
+             Logs.debug (fun k->k "how: found page url: %S" url);
+             Lwt.return (Some url)
+           | None ->
+             Logs.debug (fun k->k "how: no 'location' header");
+             Lwt.return None
+         ) else
+           Lwt.return None
+       | Error _e ->
+         Logs.debug (fun k->k "how: curl error");
+         Lwt.return None
+    )
+
 let plugin =
   [ cmd_cancer;
+    cmd_how;
   ] |> Plugin.of_cmds
