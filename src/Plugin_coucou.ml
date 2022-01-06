e020573402ceecc564ac677e037927e016ed1ce7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-01-06 11:53:07 -0500

    iel++

--- src/Plugin_coucou.ml
+++ src/Plugin_coucou.ml
@@ -78,7 +78,7 @@ let cmd_coucou state =
          let nick = if String.equal s "" then msg.Core.nick else s in
          let coucou_count = (data state nick).coucous in
          let message =
-           Printf.sprintf "%s est un coucouteur niveau %d"
+           Printf.sprintf "%s est un·e coucouteur niveau %d"
              nick coucou_count
          in
          Lwt.return (Some message)
