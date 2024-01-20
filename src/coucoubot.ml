df732e05aab82d8b3259599c1a21a8e74e2835a6
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-01-19 22:15:53 -0500

    setup prometheus for coucoubot

--- src/coucoubot.ml
+++ src/coucoubot.ml
@@ -1,6 +1,8 @@
 module C = Calculon
 module CW = Calculon_web
 module CE = Calculon_extras
+module H = Tiny_httpd
+module Prometheus = Tiny_httpd_prometheus
 
 let all_ : C.Plugin.t list =
   [
@@ -35,16 +37,63 @@ let config =
     db_file = "coucoubot.db";
   }
 
+let setup_httpd ~http_port () : H.t * Prometheus.Registry.t =
+  let server = H.create ~port:http_port () in
+
+  let reg = Prometheus.global in
+  Prometheus.instrument_server server reg;
+  Prometheus.GC_metrics.create_and_update_before_emit reg;
+
+  server, reg
+
 let () =
   Logs.set_reporter (Logs.format_reporter ());
-  try
-    (* update with CLI parameters *)
-    let config = C.Config.parse config Sys.argv in
-    Logs.set_level ~all:true (Some config.C.Config.log_level);
-    Logs.info (fun k -> k "start coucoubot");
-    C.Run_main.main config all_ |> Lwt_main.run
-  with
-  | Arg.Help msg -> print_endline msg
-  | Arg.Bad msg ->
-    prerr_endline msg;
-    exit 1
+
+  let http_port =
+    try int_of_string @@ Sys.getenv "HTTP_PORT" with _ -> 8089
+  in
+
+  let config =
+    try C.Config.parse config Sys.argv with
+    | Arg.Help msg ->
+      print_endline msg;
+      exit 0
+    | Arg.Bad msg ->
+      prerr_endline msg;
+      exit 1
+  in
+
+  (* update with CLI parameters *)
+  Logs.set_level ~all:true (Some config.C.Config.log_level);
+  Logs.info (fun k -> k "start coucoubot");
+
+  Logs.info (fun k -> k "listening on port %d" http_port);
+
+  let http_server, prom = setup_httpd ~http_port () in
+  let msg_counters =
+    List.map
+      (fun c ->
+        ( c,
+          Prometheus.Counter.create prom
+            ~tags:[ "chan", c ]
+            "coucoubot_messages" ))
+      channels
+  in
+  (* serve http in the background *)
+  let _t_server : Thread.t =
+    Thread.create (fun () -> H.run_exn http_server) ()
+  in
+
+  (* make sure to have metrics for IRC *)
+  let on_init ((module C) : Calculon.Core.t) : unit =
+    Calculon.Signal.on' C.messages (fun (msg : Irc_message.t) ->
+        (match msg.command with
+        | PRIVMSG (target, _) ->
+          (match List.assq_opt target msg_counters with
+          | None -> ()
+          | Some c -> Prometheus.Counter.incr c)
+        | _ -> ());
+        Lwt.return_unit)
+  in
+
+  Lwt_main.run @@ C.Run_main.main ~on_init config all_
